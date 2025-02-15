e5a0d774dab4ec2fd2cd0ca9682f16a47613133d
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2025-02-15 15:36:50 +0700

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Lera Savchenko on 15.02.25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
