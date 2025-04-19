3ea63e3840bcda502970d8f73641da9bc9b86b9d
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2025-04-19 16:38:18 +0300

    Added Buttons for adding / remving cards.

--- MemorizeApp/ContentView.swift
+++ MemorizeApp/ContentView.swift
@@ -8,17 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["👻", "😈", "🎃", "🕷️"]
+    let emojis = ["👻", "😈", "🎃", "🕷️", "💀", "🍭", "🧙", "🙀", "😱", "🕸️", "👹"]
+    
+    @State var cardCount = 4
     
     var body: some View {
+        VStack {
+            cards
+            cardCountAdjusters
+                .imageScale(.large)
+                .font(.largeTitle)
+        }
+        .padding()
+    }
+    
+    var cards: some View {
         HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
             }
         }
         .foregroundStyle(.orange)
-        .padding()
     }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRevomer
+            Spacer()
+            cardAdder
+        }
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+
+
+var cardRevomer: some View {
+    cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+}
+
+var cardAdder: some View {
+    cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+}
+
 }
 
 struct CardView: View {
