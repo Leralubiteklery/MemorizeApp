50e457736c1881e95e157a64218fcde3f3f2d89e
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2025-04-25 19:26:50 +0300

    Added EmojiMemoryGame ViewModel file.

---
+++ MemorizeApp/EmojiMemoryGame.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiMemoryGame.swift
+//  MemorizeApp
+//
+//  Created by Lera Savchenko on 25.04.25.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    
+    // model
+    var game: MemorizeGame<String>
+}
