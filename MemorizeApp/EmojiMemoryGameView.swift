439421137d3cee5f1d1819185a68e4503036b714
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2025-07-29 15:58:52 +0300

    Removed extra code from EmojiMemoryGameView.

--- MemorizeApp/EmojiMemoryGameView.swift
+++ MemorizeApp/EmojiMemoryGameView.swift
@@ -25,10 +25,6 @@ struct EmojiMemoryGameView: View {
                 cards
                     .animation(.default, value: gameViewModel.cards)
             }
-//            Spacer()
-//            cardThemeAdjusters
-//                .imageScale(.large)
-//                .font(.largeTitle)
             Button("Shuffle") {
                 gameViewModel.shuffle()
             }
@@ -49,40 +45,6 @@ struct EmojiMemoryGameView: View {
         }
         .foregroundStyle(.orange)
     }
-    
-//    var cardThemeAdjusters: some View {
-//        HStack {
-//            cardThemeChanger(themeName: "Flags", imageName: "globe.europe.africa.fill", selectedEmojis: flagEmojis)
-//            Spacer()
-//            cardThemeChanger(themeName: "Animals", imageName: "hare.fill", selectedEmojis: animalEmojis)
-//            Spacer()
-//            cardThemeChanger(themeName: "Gestures", imageName: "hand.raised.fill", selectedEmojis: gestureEmojis)
-//        }
-//        .padding()
-//    }
-    
-//    func cardThemeChanger(themeName: String, imageName: String, selectedEmojis: [String])  -> some View {
-//        Button {
-//            
-//            currentEmojis = (selectedEmojis + selectedEmojis).shuffled()
-//            randomCardsNumber = getRandomEvenCount(of: currentEmojis)
-//            } label: {
-//            VStack {
-//                Image(systemName: imageName)
-//                    .font(.title)
-//                Text(themeName)
-//                    .font(.body)
-//            }
-//        }
-//    }
-   
-//    func getRandomEvenCount(of cardsArray: [String]) -> Int {
-//        let evenCounts = (4...cardsArray.count).filter { $0 % 2 == 0 }
-//        guard let randomEvenCount = evenCounts.randomElement() else { return 4 }
-//        return randomEvenCount
-//    }
-    
-
 }
 
 struct CardView: View {
