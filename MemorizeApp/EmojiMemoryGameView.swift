14fb440bbd0525915068a00bfaa508d3c056edd3
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2025-08-28 16:59:44 +0300

    Added GeometryReader to the cards view.

--- MemorizeApp/EmojiMemoryGameView.swift
+++ MemorizeApp/EmojiMemoryGameView.swift
@@ -11,13 +11,15 @@ struct EmojiMemoryGameView: View {
     
     @ObservedObject var gameViewModel: EmojiMemoryGame
     
+    private let aspectRatio: CGFloat = 2/3
+
+    
     var body: some View {
         VStack {
             Text("\(gameViewModel.currentTheme.name)")
                 .font(.largeTitle)
             Text("Score: \(gameViewModel.score)")
-            ScrollView {
-                cards
+            cards
                     .animation(.default, value: gameViewModel.cards)
             }
             HStack {
@@ -33,23 +35,48 @@ struct EmojiMemoryGameView: View {
                     backgroundColor: gameViewModel.setThemeColor()
                 )
             }
-        }
         .padding()
     }
         
-    
-    var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum:85), spacing: 0)], spacing: 0) {
-            ForEach(gameViewModel.cards) { card in
-                CardView(card)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        gameViewModel.choose(card)
-                    }
+    private var cards: some View {
+        GeometryReader { geomtry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: gameViewModel.cards.count,
+                size: geomtry.size,
+                atAspectRatio: aspectRatio
+            )
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+                ForEach(gameViewModel.cards) { card in
+                    CardView(card)
+                        .aspectRatio(aspectRatio, contentMode: .fit)
+                        .padding(4)
+                        .onTapGesture {
+                            gameViewModel.choose(card)
+                        }
+                }
             }
         }
         .foregroundStyle(gameViewModel.setThemeColor())
+    }
+    
+    func gridItemWidthThatFits(
+        count: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat
+    ) -> CGFloat {
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            
+            let rowCount = (count / columnCount).rounded(.up)
+            if rowCount * height < size.height {
+                return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1
+        } while columnCount < count
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
     }
 }
 
